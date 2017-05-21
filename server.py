6a1dd58cfdf54b6521f007e64dc5ef3c6ab5206a
Jeru Mercado <jmerc87@gmail.com>
2017-05-21 22:50:40 +0800

    Add mentioning and announcing

--- server.py
+++ server.py
@@ -30,29 +30,35 @@ class ServerDataManager:
 
         if message.content == "{0} police".format(server_data.prefix):
             if True not in map(lambda x: x.name == "Vera Bot", message.author.roles):
-                await client.send_message(message.channel, "You don't own me de gozaru🚓 get the Vera Bot role first")
+                await client.send_message(message.channel,\
+                    "You don't own me de gozaru🚓 get the Vera Bot role first")
                 return
             if message.channel.id in server_data.police_channels:
                 server_data.police_channels.remove(message.channel.id)
                 self.police_refractory = 0
                 service.server_data_list.serialize()
-                await client.send_message(message.channel, "police gone de gozaru🚓")
+                await client.send_message(message.channel,\
+                    "police gone de gozaru🚓")
             else:
                 self.police_refractory = 0
                 server_data.police_channels.append(message.channel.id)
                 service.server_data_list.serialize()
-                await client.send_message(message.channel, "police active de gozaru🚓")
-        
+                await client.send_message(message.channel,\
+                    "police active de gozaru🚓")
+
         if message.content == "{0} martial".format(server_data.prefix):
             if True not in map(lambda x: x.name == "Vera Bot", message.author.roles):
-                await client.send_message(message.channel, "You don't own me de gozaru🚓 get the Vera Bot role first")
+                await client.send_message(message.channel,\
+                    "You don't own me de gozaru🚓 get the Vera Bot role first")
                 return
             server_data.militancy = not server_data.militancy
             service.server_data_list.serialize()
             if server_data.militancy:
-                await client.send_message(message.channel, "ok, will be deleting messages de gozaru🚓")
+                await client.send_message(message.channel,\
+                    "ok, will be deleting messages de gozaru🚓")
             else:
-                await client.send_message(message.channel, "ok, won't be deleting messages de gozaru🚓")
+                await client.send_message(message.channel,\
+                    "ok, won't be deleting messages de gozaru🚓")
 
         if message.channel.id in server_data.police_channels:
 
@@ -60,7 +66,8 @@ class ServerDataManager:
 
             # if militancy is active
             if (len(message.attachments) > 0 or matcher) and server_data.militancy:
-                await client.send_message(message.author, "do not post images & links. You have been warned.🚓")
+                await client.send_message(message.author,\
+                    "do not post images & links. You have been warned.🚓")
                 await client.delete_message(message)
             # control image posting
             elif self.police_refractory <= 0:
@@ -73,7 +80,9 @@ class ServerDataManager:
                 self.flammable_messages = []
 
                 if len(message.attachments) > 0 or matcher:
-                    new_message = await client.send_message(message.channel, "do not post images & links in {0} 🚓 post in #multimedia-links instead".format(message.channel.name))
+                    new_message = await client.send_message(message.channel,\
+                        "<@{0}> do not post images & links in {1} 🚓 post in #multimedia-links instead".format(
+                            message.author.id, message.channel.name))
                     self.flammable_messages.append(new_message)
                     self.police_refractory += 13
             else:
@@ -138,6 +147,16 @@ class ServerDataList:
         with open("assets/data.json", "w") as data_file:
             data_file.write(json.dumps(data))
 
+class PrivateManager:
+    def __init__(self):
+        pass
+    async def ask(self, client, service, message):
+        matcher = re.search(r"!announce (.+)", message.content)
+        if matcher:
+            announcement = matcher.group(1)
+            for data in service.server_data_list.list.values():
+                for channel in data.police_channels:
+                    await client.send_message(client.get_channel(channel), announcement)
 class Secret:
     def __init__(self):
         self.token = ""
@@ -160,6 +179,7 @@ class Service:
     def __init__(self):
         self.client = discord.Client()
         self.secret = Secret()
+        self.private_manager = PrivateManager()
         self.server_data_list = ServerDataList()
         self.manager = ServerDataManager()
 
@@ -172,7 +192,7 @@ class Service:
         @self.client.event
         async def on_message(message):
             if not message.server:
-                pass
+                await self.private_manager.ask(self.client, self, message)
             else:
                 server_data = self.server_data_list.get_server(message.server.id)
                 await self.manager.ask(server_data, self.client, self, message)
